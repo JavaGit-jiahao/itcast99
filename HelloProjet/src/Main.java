0bb4b4abcf7b710c7f9b7ad92549906d82014fbd
JavaGit-jiahao <1939240708@qq.com>
2019-08-06 21:46:54 +0800

    李四进行修改

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("我要修改数据");
+        System.out.println("李四要数据修改");
     }
 }
